03ef1aa85faa01acdd9db42c387848b34cee5156
Luan <luanchristian.rochadocarmo@gmail.com>
2023-03-22 23:31:10 -0300

    :wrench:  feat: Config cores e tipografia padrão do sistema

---
+++ monitoring-machines/src/themes/global.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import theme from "./styles";
+
+export default createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: ${theme.color.background};
+    font-family: ${theme.fontFamily};
+}
+
+`;
